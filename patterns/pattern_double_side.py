d0a00e4cf1a2e7575366aa266cc62bb71fb49c66
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-06-21 18:37:47 +0530

    corrected_approach_for_double_sided_pattern

--- patterns/pattern_double_side.py
+++ patterns/pattern_double_side.py
@@ -4,32 +4,32 @@
 # 123  321
 # 12344321
 import threading
-import multiprocessing
 
 inc_limit =4
 row_limit = 4
 col_limit = 8
-
-def inc_pattern():
+inc_pattern = []
+dec_pattern = []
+def increement_pattern():
+    global inc_pattern
     for i in range(1,row_limit+1):
-        for j in range(1,i+1):
-            print(j,end='')
-        print()
+        inc_pattern.append(''.join(str(j) for j in range(1, i + 1)))
 
-def dec_pattern():
+def decreement_pattern():
+    global dec_pattern
     for i in range(1, row_limit+1):
-        spaces = (col_limit//2)-i
-        print(" "*spaces, end='')
-        for j in range(i, 0, -1):
-            print(j, end='')
-        print()
+        spaces = col_limit - 2 * i
+        dec_pattern.append(" " * spaces + ''.join(str(j) for j in range(i, 0, -1)))
 
 if __name__ == '__main__':  
-    p1 = multiprocessing.Process(target=inc_pattern)
-    p2 = multiprocessing.Process(target=dec_pattern)
+    p1 = threading.Thread(target=increement_pattern)
+    p2 = threading.Thread(target=decreement_pattern)
     
     p1.start()
     p2.start()  
     
     p1.join()
     p2.join()
+
+    for inc,dec in zip(inc_pattern, dec_pattern):
+        print(inc+dec)
