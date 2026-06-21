3645c7f0facefd94f71411e534b2f6f82c3b7970
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-06-21 18:45:20 +0530

    basic_cont_patterns

---
+++ patterns/pattern_continuous_inc_half_pyramid.py
@@ -0,0 +1,19 @@
+# 1
+# 23
+# 456
+# 78910
+# 1112131415
+
+
+inc_limit = 5
+row_limit = 5
+k = 1
+
+for i in range(1, row_limit+1):
+    for j in range(1, i+1):
+        print(k, end='')
+        k=k+1
+    print()
+
+
+    
