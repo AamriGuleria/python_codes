83df672d532c67d1c50f249aef6e037a03b37239
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-06-27 10:47:55 +0530

    optimized array is sorted and rotated

--- warmup/check_if_array_is_sorted_and_rotated.py
+++ warmup/check_if_array_is_sorted_and_rotated.py
@@ -2,10 +2,26 @@
 
 from typing import List
 
+def optimized_approach(self, nums):
+    breaking_point = 0
+    n = len(nums)
+
+    for i in range(n - 1):
+        if nums[i] > nums[i + 1]:
+            breaking_point += 1
+            if breaking_point > 1:
+                return False
+
+    if nums[n - 1] > nums[0]:
+        return False
+
+    return True
+
 
 def check(self, nums: List[int]) -> bool:
     original_array = sorted(nums)
-    return self.rotate_to_original_array(original_array, nums)
+    # return self.rotate_to_original_array(original_array, nums)
+    return self.optimized_approach(nums)
 
 def rotate_to_original_array(self,sorted_array, array):
     array_len = len(array)
