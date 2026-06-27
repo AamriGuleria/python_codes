7505d53653f760786982731d03fef2ca74fee717
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-06-27 10:51:19 +0530

    optimized array is sorted and rotated_enhanced

--- warmup/check_if_array_is_sorted_and_rotated.py
+++ warmup/check_if_array_is_sorted_and_rotated.py
@@ -2,6 +2,18 @@
 
 from typing import List
 
+def optimized_v2(self,nums):
+    n = len(nums)
+    count = 0
+
+    for i in range(n):
+        if nums[i] > nums[(i + 1) % n]:
+            count += 1
+            if count > 1:
+                return False
+
+    return True
+
 def optimized_approach(self, nums):
     breaking_point = 0
     n = len(nums)
