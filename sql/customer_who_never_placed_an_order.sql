fce7d7dbee56afdf381a766025aecf2fb42690f9
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-01 18:36:32 +0530

    not_exists

--- sql/customer_who_never_placed_an_order.sql
+++ sql/customer_who_never_placed_an_order.sql
@@ -5,3 +5,11 @@ FROM customers
 WHERE customer_id NOT IN (
     SELECT DISTINCT customer_id FROM orders
 );
+
+
+-- Exists/Not Exists approach
+SELECT customer_id, first_name, last_name, age, country
+FROM customers c
+WHERE NOT EXISTS (
+    SELECT 1 FROM orders o WHERE o.customer_id = c.customer_id
+)
