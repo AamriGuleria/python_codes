ef0e65a9a5a055568649bdf833d07d3de98678b2
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-02 11:17:06 +0530

    optimized_approach_majority_element_using_Boyer_moore_voting

--- warmup/majority_element.py
+++ warmup/majority_element.py
@@ -19,6 +19,31 @@ def majorityElement(self, nums: List[int]) -> List[int]:
             final_list.append(k)
     return final_list
 
+#  Boyer-Moore Voting (O(n) time, O(1) space)
+def optimizedMajorityElement(self, nums: List[int]) -> List[int]:
+    length = len(nums)
+    max_freq = length // 3
+    candidate1 = candidate2 = None
+    count1 = count2 = 0
+    for num in nums:
+        if candidate1 == num:
+            count1 += 1
+        elif candidate2 == num:
+            count2 += 1
+        elif count1 == 0:
+            candidate1, count1 = num, 1
+        elif count2 == 0:
+            candidate2, count2 = num, 1
+        else:
+            count1 -= 1
+            count2 -= 1
+
+    result = []
+    for c in (candidate1, candidate2):
+        if c is not None and nums.count(c) > max_freq:
+            result.append(c)
+    return result
+
 if __name__ == "__main__":
     array = str(input("Enter the array elements: "))
     array = array.split()
