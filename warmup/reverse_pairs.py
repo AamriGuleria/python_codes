92da2fb898b0e1e5ca7e88423992a9ac5fa02c14
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-04 11:22:21 +0530

    merge sort apporach for reverse pairs

--- warmup/reverse_pairs.py
+++ warmup/reverse_pairs.py
@@ -12,7 +12,46 @@ def reversePairs(self, nums: List[int]) -> int:
     return count
 
 
+class mergeSortApproach:
+    
+    def reversePairs(self, nums: List[int]) -> int:
+        self.count = 0
+        self.divide(nums)
+        return self.count
+    # Divide the arrays
+    def countPairs(self, left, right):
+        j = 0
+        for i in range(len(left)):
+            while j < len(right) and left[i] > 2 * right[j]:
+                j += 1
+            self.count += j
+    def divide(self, nums):
+        n = len(nums)
+        if n <= 1:
+            return nums
+        mid = n//2
+        left = self.divide(nums[:mid])
+        right = self.divide(nums[mid:])
+        self.countPairs(left,right)
+        return self.merge(left,right)
+    # Merge the two sorted arrays
+    def merge(self,left,right):
+        i=0
+        j=0
+        result = []
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                result.append(left[i])
+                i=i+1
+            else:
+                result.append(right[j])
+                j=j+1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+
 if __name__ == "__main__":
     array = str(input("Enter the array elements: "))
     array = array.split()
-    reversePairs(array)
+    service = mergeSortApproach()
+    service.reversePairs(array)
