acb5008144a4b24b92d0bbd9d40c3e0de1ed5722
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-05 12:19:42 +0530

    a little optimized

--- warmup/sum_of_beauty_of_all_substrings.py
+++ warmup/sum_of_beauty_of_all_substrings.py
@@ -8,14 +8,24 @@ class Solution:
         max_val,min_val = max(values),min(values)
         return max_val-min_val
 
+    # def beautySum(self, s: str) -> int:
+    #     n=len(s)
+    #     for i in range(n-1):
+    #         sub = s[i]
+    #         for j in range(i+1,n):
+    #             sub=sub+s[j]
+    #             self.counter+=self.beauty(sub)
+    #     return self.counter
     def beautySum(self, s: str) -> int:
-        n=len(s)
-        for i in range(n-1):
-            sub = s[i]
-            for j in range(i+1,n):
-                sub=sub+s[j]
-                self.counter+=self.beauty(sub)
-        return self.counter
+        n = len(s)
+        total = 0
+        for i in range(n - 1):
+            count = Counter(s[i])
+            for j in range(i + 1, n):
+                count[s[j]] += 1
+                values = count.values()
+                total += max(values) - min(values)
+        return total
 
 if __name__ == "__main__":
     string = str(input("Enter your string: "))
