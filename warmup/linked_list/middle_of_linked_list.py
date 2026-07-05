d6787f0312fc34fd91cc25b1c7a40c02ec6fd265
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-05 14:16:41 +0530

    middle of linked list

---
+++ warmup/linked_list/middle_of_linked_list.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+        n=1
+        while node.next != None:
+            node = node.next
+            n=n+1
+        print(f"Length of the linked list {n}")
+        i=0
+        while i!=n//2 and head is not None:
+            head=head.next
+            i=i+1
+        return head
