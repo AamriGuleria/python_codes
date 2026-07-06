4b104ed168483a94e81eb92266e6926431365673
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-06 11:28:15 +0530

    optimized reverse linked list

--- warmup/linked_list/reverse_linked_list.py
+++ warmup/linked_list/reverse_linked_list.py
@@ -18,3 +18,11 @@ class Solution:
             new_list.next = ListNode(v)
             new_list = new_list.next
         return final_list.next
+
+    def optimizedReverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        new_head = self.reverseList(head.next) 
+        head.next.next = head
+        head.next = None
+        return new_head  
