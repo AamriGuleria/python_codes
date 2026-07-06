3ff9b25e2de1f77a553738b196a42f40fe8a0fbe
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-06 11:48:26 +0530

    brute force has cycle problem

---
+++ warmup/linked_list/linked_list_cycle.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = []
+        temp = head
+        while temp is not None:
+            if temp in seen:
+                return True
+            seen.append(temp)
+            temp=temp.next
+        return False
