34e5860d67e2d885b5a563fb216d53c94d325c78
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-07 11:02:24 +0530

    optimized using two pointee

--- warmup/linked_list/delete_last_nth_node.py
+++ warmup/linked_list/delete_last_nth_node.py
@@ -23,3 +23,17 @@ class Solution:
             new_ptr.next = ListNode(lst[i])
             new_ptr = new_ptr.next
         return final_ptr.next
+    
+    # First let fast travel n distance then fast travels L-n distance and slow completes same distance from beginning
+    def optimizedRemoveNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0,head)
+        fast = slow = dummy
+        for i in range(n):
+            fast=fast.next
+        
+        while fast.next:
+            fast=fast.next
+            slow= slow.next
+        slow.next = slow.next.next
+        return dummy.next
+ 
