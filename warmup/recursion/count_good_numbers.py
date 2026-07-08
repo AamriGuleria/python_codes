014b3dadc2f8bd2b07fc441f5c56fc51b9f243d8
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-08 12:41:05 +0530

    count good numbers brute force

---
+++ warmup/recursion/count_good_numbers.py
@@ -0,0 +1,24 @@
+class Solution:
+    def is_prime(self,n):
+        return n in (2, 3, 5, 7)
+    def isGood(self,n):
+        for i,ch in enumerate(n):
+            ch = int(ch)
+            if i%2==0 and ch%2!=0:
+                #Even position
+                return False
+            elif i%2!=0 and not self.is_prime(ch):
+                return False
+        return True
+
+    def countGoodNumbers(self, n: int) -> int:
+        count=0
+        if n == 1:
+            my_range = range(0, 10)
+        else:
+            my_range = range(10**(n-1), 10**n)
+        for i in my_range:
+            s = str(i).zfill(n)
+            if self.isGood(str(s)):
+                count=count+1
+        return count
