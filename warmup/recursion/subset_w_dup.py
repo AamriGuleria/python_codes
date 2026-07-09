67ef0289b8df2495413317665efad8f942c01031
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-09 14:21:22 +0530

    subset w dup

---
+++ warmup/recursion/subset_w_dup.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        final=[]
+        nums.sort()
+        def recursive_appraoch(start,temp):
+            final.append(temp[:])
+            for i in range(start,n):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                temp.append(nums[i])
+                recursive_appraoch(i+1,temp)
+                temp.pop()
+
+        recursive_appraoch(0,[])
+        return final
