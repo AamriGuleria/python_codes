d31488d7c6f5a3f651b638dd23ba1f5e69352684
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-11 13:08:33 +0530

    prime_number_till_n_brute_force

---
+++ warmup/prime_number_till_n.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        count = 0
+        for i in range(2, n):
+            is_prime = True
+            for j in range(2, i//2+1):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                count += 1
+        return count
