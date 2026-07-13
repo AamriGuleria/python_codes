7807ca83ec2476acd7f3b8f2511314bd169757f0
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-13 13:26:50 +0530

    next greater element 2 brute force

---
+++ warmup/stack_n_queues/next_greater_elements_2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        stack = [-1]*n
+        i=n-1
+        while i>=0:
+            j = (i + 1) % n
+            while i!=j:
+                if nums[j]>nums[i]:
+                    stack[i]=nums[j]
+                    break
+                j = (j + 1) % n
+            i=i-1
+        return stack
+            
