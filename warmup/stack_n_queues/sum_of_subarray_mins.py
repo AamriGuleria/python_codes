bc42b4e5602413c202de8be183177a2883231b45
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-14 13:51:26 +0530

    sum of subarray mins brute force

---
+++ warmup/stack_n_queues/sum_of_subarray_mins.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        n = len(arr)
+        total = 0
+        for i in range(n):
+            for j in range(i, n):
+                total += min(arr[i:j+1])
+        return total
