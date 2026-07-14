aea84672e22c148df151c22995d46364d1405118
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-14 13:09:16 +0530

    optimized next element problem

--- warmup/stack_n_queues/next_greater_elements_2.py
+++ warmup/stack_n_queues/next_greater_elements_2.py
@@ -14,3 +14,15 @@ class Solution:
             i=i-1
         return stack
             
+    def optimizedNextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [-1]*n
+        stack=[]
+        for i in range(2*n):
+            idx = i%n
+            while stack and nums[idx] > nums[stack[-1]]:
+                result[stack.pop()] = nums[idx]
+            if i<n:
+                stack.append(idx)
+        return result
+            
