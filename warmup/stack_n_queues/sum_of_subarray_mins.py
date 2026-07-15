6d07035bea2937a13bbf26667e5d3cb069a9633c
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-15 11:54:04 +0530

    sum of subarrays mins little optimized to O(n2)

--- warmup/stack_n_queues/sum_of_subarray_mins.py
+++ warmup/stack_n_queues/sum_of_subarray_mins.py
@@ -7,3 +7,13 @@ class Solution:
             for j in range(i, n):
                 total += min(arr[i:j+1])
         return total
+
+    def optimizedSumSubarrayMins(self, arr: List[int]) -> int:
+        n = len(arr)
+        total = 0
+        for i in range(n):
+            curr_min = arr[i]
+            for j in range(i, n):
+                curr_min = min(curr_min, arr[j])
+                total += curr_min
+        return total
