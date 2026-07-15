f6d4d51cfd2fd585023f58c5230a8bec1a5a1802
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-15 13:09:06 +0530

    sum of subarrays mins little optimized to O(n)

--- warmup/stack_n_queues/sum_of_subarray_mins.py
+++ warmup/stack_n_queues/sum_of_subarray_mins.py
@@ -17,3 +17,17 @@ class Solution:
                 curr_min = min(curr_min, arr[j])
                 total += curr_min
         return total
+
+    def mostOptimalSumSubarrayMins(self, arr: List[int]) -> int:
+        MOD = 10**9 + 7
+        n = len(arr)
+        total = 0
+        stack = []
+        for i in range(n + 1): 
+            curr = arr[i] if i < n else float('-inf') 
+            while stack and arr[stack[-1]] >= curr:
+                mid = stack.pop()
+                left_bound = stack[-1] if stack else -1
+                total += arr[mid] * (mid - left_bound) * (i - mid)
+            stack.append(i)
+        return total % MOD
