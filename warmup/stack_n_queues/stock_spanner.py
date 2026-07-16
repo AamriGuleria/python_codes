2eede204c317b4fd50314cfd91b8c4d0e1adb018
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-16 13:43:13 +0530

    optimized force stock spanner

--- warmup/stack_n_queues/stock_spanner.py
+++ warmup/stack_n_queues/stock_spanner.py
@@ -1,15 +1,26 @@
 class StockSpanner:
 
+    # def __init__(self):
+    #     self.nums = []
+
+    # def next(self, price: int) -> int:
+    #     current = price
+    #     count = 1
+    #     n = len(self.nums)
+    #     for i in range(n-1,-1,-1):
+    #         if price<self.nums[i]:
+    #             break
+    #         count=count+1
+    #     self.nums.append(price)
+    #     return count
+
     def __init__(self):
-        self.nums = []
+        self.stack = []  
 
     def next(self, price: int) -> int:
-        current = price
-        count = 1
-        n = len(self.nums)
-        for i in range(n-1,-1,-1):
-            if price<self.nums[i]:
-                break
-            count=count+1
-        self.nums.append(price)
-        return count
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            _, prev_span = self.stack.pop()
+            span += prev_span
+        self.stack.append((price, span))
+        return span
