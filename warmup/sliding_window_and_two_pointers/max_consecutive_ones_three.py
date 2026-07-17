46ab1df0bd2be28c19c71ab7d43abd566ba49a4c
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-17 13:46:18 +0530

    step two optimization/max_consecutive ones

--- warmup/sliding_window_and_two_pointers/max_consecutive_ones_three.py
+++ warmup/sliding_window_and_two_pointers/max_consecutive_ones_three.py
@@ -1,5 +1,6 @@
 
 from typing import List
+from collections import deque
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         last_seen = []
@@ -12,6 +13,21 @@ class Solution:
             if len(last_seen) > k:
                 l = last_seen[0] + 1  
                 last_seen.pop(0)      
+            max_len = max(max_len, r - l + 1)
+            r += 1
+        return max_len
+    
+    def optimalLongestOnes(self, nums: List[int], k: int) -> int:
+        last_seen = deque()
+        l = r = 0
+        n = len(nums)
+        max_len = 0
+        while r < n:
+            if nums[r] == 0:
+                last_seen.append(r)
+            if len(last_seen) > k:
+                l = last_seen[0] + 1  
+                last_seen.popleft()    
             max_len = max(max_len, r - l + 1)
             r += 1
         return max_len
