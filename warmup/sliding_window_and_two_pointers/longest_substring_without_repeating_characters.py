8a6e4eee3a5ffb862bf37db9dff8d02c39c8318d
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-17 12:32:49 +0530

    brute_force/ sliding window and two pointers approach

--- warmup/sliding_window_and_two_pointers/longest_substring_without_repeating_characters.py
+++ warmup/sliding_window_and_two_pointers/longest_substring_without_repeating_characters.py
@@ -10,3 +10,17 @@ class Solution:
                 unique_set.add(s[j])   
                 max_len = max(max_len, len(unique_set)) 
         return max_len
+    
+    # optimized approach - sliding window and two pointers
+    def optimizedLengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        max_len = 0
+        l = 0
+        window = set()
+        for r in range(n):
+            while s[r] in window:
+                window.remove(s[l])
+                l += 1
+            window.add(s[r])
+            max_len = max(max_len, r - l + 1)
+        return max_len
