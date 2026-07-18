ad872f13d878a51b312ad94b8e01320eb5bf93f2
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-18 11:53:57 +0530

    brute_force/maximum points to be obtained by cards

---
+++ warmup/sliding_window_and_two_pointers/maximum_points_to_be_obtained_by_cards.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        total = sum(cardPoints)
+        window = n-k
+        if window == 0:
+            return total
+        min_sub = None
+        i = 0
+        for i in range(k):
+            sub = sum(cardPoints[i:i+window])
+            min_sub = sub if min_sub == None else min(min_sub,sub)
+        return total-min_sub
+            
