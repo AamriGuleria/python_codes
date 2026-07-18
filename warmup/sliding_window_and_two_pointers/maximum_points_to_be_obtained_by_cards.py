e777e56988696c3eeceac483044a87737453676e
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-18 12:12:39 +0530

    optimized/maximum points to be obtained by cards

--- warmup/sliding_window_and_two_pointers/maximum_points_to_be_obtained_by_cards.py
+++ warmup/sliding_window_and_two_pointers/maximum_points_to_be_obtained_by_cards.py
@@ -13,3 +13,17 @@ class Solution:
             min_sub = sub if min_sub == None else min(min_sub,sub)
         return total-min_sub
             
+
+    def optimalMaxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        total = sum(cardPoints)
+        window = n-k
+        if window == 0:
+            return total
+        cur = sum(cardPoints[:window])
+        min_sub = cur
+        i = 0
+        for i in range(1,k+1):
+            cur += cardPoints[i + window - 1] - cardPoints[i - 1]
+            min_sub = min(min_sub, cur)
+        return total-min_sub
