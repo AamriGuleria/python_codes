bf70d23787f71b2fe51ca618c475a5f3f37ad8a8
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-19 13:34:28 +0530

    optimized version/number of substrings containing all elements

--- warmup/sliding_window_and_two_pointers/number_of_substrings_containing_abc_elements.py
+++ warmup/sliding_window_and_two_pointers/number_of_substrings_containing_abc_elements.py
@@ -1,4 +1,7 @@
+
+
 class Solution:
+    # Gives TLE with O(n2)
     def numberOfSubstrings(self, s: str) -> int:
         n = len(s)
         count = 0
@@ -9,3 +12,18 @@ class Solution:
                 if len(sub) == 3:      
                     count += 1
         return count
+    # Improved version
+    def numberOfSubstrings(self, s: str) -> int:
+        n = len(s)
+        count = {}
+        l=0
+        res = 0
+        for r in range(len(s)):
+            count[s[r]] = count.get(s[r], 0) + 1
+            while len(count) == 3:
+                count[s[l]] -= 1
+                if count[s[l]] == 0:
+                    del count[s[l]]
+                l+=1
+            res += l
+        return res
