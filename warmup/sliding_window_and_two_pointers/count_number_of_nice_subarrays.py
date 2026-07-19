106823ee9aa78d9296b1068a6a811229e1d5a575
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-19 12:44:03 +0530

    brute force step 2/ count number of nice subarrays reduce time complexity from n3 to n2

--- warmup/sliding_window_and_two_pointers/count_number_of_nice_subarrays.py
+++ warmup/sliding_window_and_two_pointers/count_number_of_nice_subarrays.py
@@ -15,3 +15,15 @@ class Solution:
                 if count_odd(nums[i:j+1]):
                     result+=1
         return result
+    
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        n=len(nums)
+        result=0
+        for i in range(n):
+            odd_count=0
+            for j in range(i,n):
+                if nums[j] % 2 != 0:
+                    odd_count += 1
+                if odd_count == k:
+                    result += 1
+        return result
