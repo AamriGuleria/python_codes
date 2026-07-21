5e78472ba479ec5196382728a3bf58e571fd8e80
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-21 14:13:56 +0530

    top k freq elements

--- warmup/heap/top_k_frequent_elements.py
+++ warmup/heap/top_k_frequent_elements.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from typing import List
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = Counter(nums)
@@ -8,3 +9,15 @@ class Solution:
         for tup in result:
             lst.append(tup[0])
         return lst
+    
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        heap = []
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+            
+        return [num for count, num in heap]
+        
