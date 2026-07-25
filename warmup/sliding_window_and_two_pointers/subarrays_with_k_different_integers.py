3104a54196da0935643fa5cf9287a586c85cba59
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-25 11:51:33 +0530

    good_initial_solution / subarrays_with_k_different_integers

---
+++ warmup/sliding_window_and_two_pointers/subarrays_with_k_different_integers.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        def atMost(k):
+            if k == 0:
+                return 0
+            count = Counter()
+            res = 0
+            l = 0
+            for r in range(len(nums)):
+                count[nums[r]] += 1
+                while len(count) > k:
+                    count[nums[l]] -= 1
+                    if count[nums[l]] == 0:
+                        del count[nums[l]]
+                    l=l+1
+                res += r - l + 1
+            return res
+
+        return atMost(k) - atMost(k - 1)
+        
