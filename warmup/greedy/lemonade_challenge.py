d7b0217c2ff96478918b707e657b64b2f3ceef58
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-26 17:53:36 +0530

    corrected_solution/lemonade_challenge

--- warmup/greedy/lemonade_challenge.py
+++ warmup/greedy/lemonade_challenge.py
@@ -17,3 +17,24 @@ class Solution:
                 value=False
                 break
         return value
+
+    def correctedLemonadeChange(self, bills: List[int]) -> bool:
+        five=ten=0
+        for b in bills:
+            if b==5:
+                five+=1
+            elif b==10:
+                if five==0:
+                    return False
+                five-=1
+                ten+=1
+            else:
+                if five>0 and ten>0:
+                    five-=1
+                    ten-=1
+                elif five>=3:
+                    five-=3
+                else:
+                    return False
+        return True
+            
