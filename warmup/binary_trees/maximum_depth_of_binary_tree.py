b560588ef393317a2cbf7f7f4a30f008058b81d8
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-29 12:50:00 +0530

    initial/maximum_depth_of_bt

---
+++ warmup/binary_trees/maximum_depth_of_binary_tree.py
@@ -0,0 +1,16 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def recursive_approach(self,root,level):
+        if root == None:
+            return 0
+        if root.left == None and root.right == None:
+            return level
+
+        return max(self.recursive_approach(root.left,level+1),self.recursive_approach(root.right,level+1))
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self.recursive_approach(root,1)
