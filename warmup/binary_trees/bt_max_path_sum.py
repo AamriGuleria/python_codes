0adf2ac1701cf54a3b984845e29abd044429a852
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-07-31 13:02:52 +0530

    initial/bt_max_path_sum-corrected

--- warmup/binary_trees/bt_max_path_sum.py
+++ warmup/binary_trees/bt_max_path_sum.py
@@ -6,14 +6,24 @@ class TreeNode:
         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        total=0
-        max_total = 0
         def recursive_approach(root):
             if root==None:
                 return 0
             if root.val>=0:
                 return root.val+recursive_approach(root.left)+recursive_approach(root.right)
             else:
-                # max_total = max(max_total,total)
                 return max(recursive_approach(root.left),recursive_approach(root.right))
         return recursive_approach(root)
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.max_total = float('-inf')
+        def recursive_approach(root):
+            if root==None:
+                return 0
+            left = max(recursive_approach(root.left), 0)
+            right = max(recursive_approach(root.right), 0)
+            self.max_total = max(self.max_total, root.val+left+right)
+            return root.val+ max(left, right)
+        recursive_approach(root)
+        return self.max_total
