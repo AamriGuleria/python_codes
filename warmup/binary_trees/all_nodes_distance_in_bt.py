c85f6e084cb511f654f2359919368395dfad2693
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-04 13:06:16 +0530

    all_nodes_distance_in_bt/optimized

--- warmup/binary_trees/all_nodes_distance_in_bt.py
+++ warmup/binary_trees/all_nodes_distance_in_bt.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import deque
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -23,3 +24,30 @@ class Solution:
             
         recursive_approach(root,0,False)
         return lst
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        parent_child = {}
+        def build_parent(node,parent):
+            if not node:
+                return
+            parent_child[node]=parent
+            build_parent(node.left,node)
+            build_parent(node.right,node)
+        build_parent(root,None)
+
+        # BFS from target, treating parent/left/right as neighbors
+        visited = set()
+        queue = deque([target])
+        visited.add(target)
+        distance = 0
+        while queue:
+            if k == distance:
+                return [n.val for n in queue]
+            for _ in range(len(queue)):
+                elem = queue.popleft()
+                for neighbor in (elem.left,elem.right,parent_child[elem]):
+                    if neighbor and neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append(neighbor)
+            distance += 1
+        return []
