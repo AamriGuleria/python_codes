d05b350f142e4d0a7cc9606bd320186a90e2f812
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-04 12:00:00 +0530

    count_complete_tree_nodes/best

--- warmup/binary_trees/count_complete_tree_nodes.py
+++ warmup/binary_trees/count_complete_tree_nodes.py
@@ -24,3 +24,34 @@ class Solution:
                 return 0
             return 1+recursive_approach(node.left)+recursive_approach(node.right)
         return recursive_approach(root)
+
+# Instead of counting each node , find if a subtree is perfect binary and find count using formula otherwise recursively traverse 
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        def get_left_height(node):
+            h = 0
+            while node:
+                h += 1
+                node = node.left
+            return h
+        def get_right_height(node):
+            h = 0
+            while node:
+                h += 1
+                node = node.right
+            return h
+
+        def count(node):
+            if not node:
+                return 0
+            
+            left_h = get_left_height(node)
+            right_h = get_right_height(node)
+
+            if left_h == right_h:
+                return (2**left_h)-1
+            
+            else:
+                return 1+count(node.left)+count(node.right)
+
+        return count(root)
