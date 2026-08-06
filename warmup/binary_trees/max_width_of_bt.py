bc0cae67a283acdc5411403038af70f6cb69ca9f
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-06 13:10:20 +0530

    max_width_of_bt

---
+++ warmup/binary_trees/max_width_of_bt.py
@@ -0,0 +1,31 @@
+from collections import deque
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        max_width = 0
+        queue = deque([(root, 0)])
+        while queue:
+            level_length = len(queue)
+            _,first_index = queue[0]
+            for i in range(level_length):
+                node, index = queue.popleft()
+                if i == level_length - 1:
+                    last_index = index
+                if node.left:
+                    queue.append((node.left, 2 * index))
+                if node.right:
+                    queue.append((node.right, 2 * index + 1))    
+            max_width = max(max_width, last_index - first_index + 1)
+        return max_width
+
+
+            
