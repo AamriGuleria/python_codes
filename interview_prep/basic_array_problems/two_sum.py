f91edd38c8dfca4014117f07f0a55f0bcebc3bbb
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-09 12:36:58 +0530

    optimized_for_two_sum

--- interview_prep/basic_array_problems/two_sum.py
+++ interview_prep/basic_array_problems/two_sum.py
@@ -8,3 +8,14 @@ class Solution:
                 if nums[i]+nums[j]==target:
                     return sorted([i,j])
         return [-1,-1]
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        n = len(nums)
+        for i in range(n):
+            second_element = target-nums[i]
+            if second_element in seen:
+                return [seen[second_element],i]
+            seen[nums[i]]=i
+        return [-1,-1]
