90421cc151fd88473dd40f63dcf2566812c68705
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-09 18:23:01 +0530

    max_subarray/brute_force_w_TLE

---
+++ interview_prep/basic_array_problems/max_subarray.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        if len(nums)==1:
+            return nums[0]
+        max_sum = float("-inf")
+        for i in range(n):
+            for j in range(i+1,n+1):
+                max_sum = max(max_sum,sum(nums[i:j]))
+
+        return max_sum
