b548b02c32f0c21c82961c4b9e7012b601b19d55
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-09 18:40:38 +0530

    max_subarray/best_kadanes_algo

--- interview_prep/basic_array_problems/max_subarray.py
+++ interview_prep/basic_array_problems/max_subarray.py
@@ -10,3 +10,13 @@ class Solution:
                 max_sum = max(max_sum,sum(nums[i:j]))
 
         return max_sum
+
+# Kadane's algo , start new or continue prev
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        curr_sum = max_sum = nums[0]
+        for i in range(1,n):
+            curr_sum = max(nums[i] , curr_sum+nums[i])
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
