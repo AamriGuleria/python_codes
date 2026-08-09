ed7925bdb361c42246d76a9810adb7e7317da2df
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-09 13:36:02 +0530

    linear approach/best_time_to_buy_n_sell_the_stock

--- interview_prep/basic_array_problems/best_time_to_buy_and_sell_stock.py
+++ interview_prep/basic_array_problems/best_time_to_buy_and_sell_stock.py
@@ -10,3 +10,15 @@ class Solution:
                     max_profit = prices[j]-prices[i]
 
         return max_profit
+
+# Improved O(N) approach
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minimum_buy_price=prices[0]
+        max_profit=0
+        n = len(prices)
+        for i in range(n):
+            max_profit = max(max_profit,prices[i]-minimum_buy_price)
+            if prices[i]<minimum_buy_price:
+                minimum_buy_price = prices[i]
+        return max_profit
