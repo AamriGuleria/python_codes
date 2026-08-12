42d7c9b28aff15c9b34080a04d880cc739dd528f
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-12 12:34:48 +0530

    two_sum_bst/initial_solution

---
+++ warmup/BST/two_sum_in_bst.py
@@ -0,0 +1,18 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        seen = []
+        def recursive_approach(node):
+            if node == None:
+                return False
+            if k-node.val in seen:
+                return True
+            seen.append(node.val)
+            return recursive_approach(node.left) or recursive_approach(node.right)
+            return False
+        return recursive_approach(root)
