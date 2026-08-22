fd7bef84a4ff73564bd2d8d614524eee46936669
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-22 13:29:56 +0530

    broadcast variable

---
+++ pyspark/broadcast_variable.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("BroadCast_Variable").getOrCreate()
+sc = spark.sparkContext
+# Without Broadcast
+lookup = {"US": "United States", "UK": "United Kingdom"}
+# Create sample RDD
+data = ["US", "UK", "US", "IN", "UK", "CA"]
+rdd = sc.parallelize(data)
+
+rdd.map(lambda x: (x,lookup.get(x,'Unknown')))
+
+
+# With Lookup
+broadcast_lookup = sc.broadcast(lookup)
+# rdd.map(lambda x: (x,broadcast_lookup.value.get(x, "Unknown")))
+result_with_broadcast = rdd.map(lambda x: (x, broadcast_lookup.value.get(x, "Unknown"))).collect()
+
+print("With Broadcast Result:")
+for item in result_with_broadcast:
+    print(item)
+
+# 2. Cleanup (Good practice to free memory)
+broadcast_lookup.unpersist()
+
+# 3. Stop the session
+spark.stop()
