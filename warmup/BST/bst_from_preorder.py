9dc0a1959397e3da6242ece73db84c4c621674fa
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-08-25 13:09:33 +0530

    bst/optimized_solution_build_bst_from_preorder

--- warmup/BST/bst_from_preorder.py
+++ warmup/BST/bst_from_preorder.py
@@ -34,3 +34,20 @@ class Solution:
             root.right = recursive_approach(right)
             return root
         return recursive_approach(preorder)
+
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        root = TreeNode(preorder[0])
+        stack = [root]
+        for val in preorder[1:]:
+            node = TreeNode(val)
+            if val < stack[-1].val:
+                stack[-1].left = node
+            else:
+                parent = stack[-1]
+                while stack and stack[-1].val < val:
+                    parent = stack.pop()
+                parent.right = node
+            stack.append(node)
+        return root
