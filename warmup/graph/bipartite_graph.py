bbbd5f2729917812c00ad2f354efd5e1166bbbe8
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2026-09-04 14:02:24 +0530

    best_optimal/using dfs

--- warmup/graph/bipartite_graph.py
+++ warmup/graph/bipartite_graph.py
@@ -18,3 +18,33 @@ class Solution:
                     B.add(j)
 
         return True
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        A = set()
+        B = set()
+        n = len(graph)
+        for node in range(n):
+            if node in A or node in B:
+                continue
+            A.add(node)
+            stack = [node]
+
+            while stack:
+                node = stack.pop()
+                if node in B:
+                    for j in graph[node]:
+                        if j in B:
+                            return False
+                        if j not in A:
+                            A.add(j)
+                            stack.append(j)
+
+                else:
+                    for j in graph[node]:
+                        if j in A:
+                            return False
+                        if j not in B:
+                            B.add(j)
+                            stack.append(j)
+        return True
